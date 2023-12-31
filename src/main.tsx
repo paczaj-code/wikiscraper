d4ce615a6800f61aaf72e7e1ab44bef16cea36b3
paczaj-code <marcinpaczkowski@icloud.com>
2023-12-31 12:30:47 +0100

    Add browser

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Route } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+       <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/artistlist" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
