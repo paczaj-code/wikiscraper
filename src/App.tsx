36c62d220ec9b00b357642f302bfb98a79de0dfa
paczaj-code <marcinpaczkowski@icloud.com>
2023-12-31 12:07:02 +0100

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
